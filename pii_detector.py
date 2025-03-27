a4ca276b918b13d9f70ebd29709be4eb148fb6af
ingeol.song <ingeol.song@epapyrus.com>
2025-03-27 18:54:10 +0900

    feat: 프롬프트를 좀 더 엄격하게 작성, 시스템 프롬프트, human 프롬프트 구분

--- pii_detector.py
+++ pii_detector.py
@@ -1,8 +1,7 @@
-from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import PydanticOutputParser
-
 from dotenv import load_dotenv
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 
 from models import Pii
 
@@ -10,7 +9,14 @@ load_dotenv()
 
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
-prompt = PromptTemplate.from_file('prompts/pii_detector.txt', encoding='utf-8')
+system_template = SystemMessagePromptTemplate.from_template(
+    "당신은 한국 개인정보 식별 전문가입니다. 정확한 개인정보 형식만 인식하세요. 형식이 일치하지 않으면 절대 포함하지 마세요. 틀리면 작업 실패로 간주됩니다."
+)
+human_template = HumanMessagePromptTemplate.from_template(
+    open("prompts/pii_detector.txt", encoding="utf-8").read()
+)
+
+prompt = ChatPromptTemplate.from_messages([system_template, human_template])
 output_parser = PydanticOutputParser(pydantic_object=Pii)
 
 chain = prompt | llm | output_parser
