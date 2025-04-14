10beb44466468e1ad7ed476053da0814bb2a5303
ingeol.song <ingeol.song@epapyrus.com>
2025-04-14 11:52:08 +0900

    feat: uv로 변경, gemini 추가, 로컬 LLM 제거

--- llm_model.py
+++ llm_model.py
@@ -1,35 +1,15 @@
 import os
 
-import torch
-from langchain_huggingface import HuggingFacePipeline
-
 from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from transformers import AutoModelForCausalLM, BitsAndBytesConfig, pipeline, AutoTokenizer
 
 load_dotenv()
 
 model = os.getenv('LLM_MODEL')
 print(f"Model: {model}")
 if model.startswith('gpt'):
-    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
-else:
-    print(f"Device: {torch.device('cuda' if torch.cuda.is_available() else 'cpu')}")
-    tokenizer = AutoTokenizer.from_pretrained(model)
-    llm_model = AutoModelForCausalLM.from_pretrained(
-        model,
-        device_map="auto",
-        quantization_config=BitsAndBytesConfig(
-            load_in_4bit=True,
-        ),
-        torch_dtype=torch.bfloat16,
-        low_cpu_mem_usage=True,
-    )
-    hf_pipeline = pipeline(
-        task="text-generation",
-        model=llm_model,
-        tokenizer=tokenizer,
-        do_sample=False,
-        max_new_tokens=512,
-    )
-    llm = HuggingFacePipeline(pipeline=hf_pipeline)
+    llm = ChatOpenAI(model=model, temperature=0)
+elif model.startswith('gemini'):
+    llm = ChatGoogleGenerativeAI(model=model, temperature=0)
+    
